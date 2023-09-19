521e2fcef389f8f6d569df7673492a6e49f7bede
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-19 13:32:44 +0545

    feat: create course ui

--- app/(dashboard)/teacher/courses/page.tsx
+++ app/(dashboard)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Courses;

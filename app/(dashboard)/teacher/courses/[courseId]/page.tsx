e8c54291b84f2f46e58ab82224f9d2595d8b3281
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-20 08:10:52 +0545

    feat: edit category

--- app/(dashboard)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseId = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -14,6 +15,11 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
   if (!userId) return redirect("/");
 
   const course = await db.course.findUnique({ where: { id: params.courseId } });
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   if (!course) return redirect("/");
 
@@ -42,6 +48,14 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
           <TitleForm title={course.title} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
         </div>
       </div>
     </div>

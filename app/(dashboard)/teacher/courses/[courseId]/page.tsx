3ae0e3e9f29dd579f1c585c0658b986a38b165b4
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-20 08:51:28 +0545

    feat: attachments add and delete

--- app/(dashboard)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 
 import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
@@ -9,13 +9,23 @@ import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseId = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
 
   if (!userId) return redirect("/");
 
-  const course = await db.course.findUnique({ where: { id: params.courseId } });
+  const course = await db.course.findUnique({
+    where: { id: params.courseId },
+    include: {
+      attachments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
@@ -74,6 +84,14 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
               <h2 className="text-xl">Sell your course</h2>
             </div>
             <PriceForm initialData={course} courseId={course.id} />
+          </div>
+
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">Resources & Attachments</h2>
+            </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
           </div>
         </div>
       </div>

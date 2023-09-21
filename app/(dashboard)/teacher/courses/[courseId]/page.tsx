b57deb38bebdc319d1930f1d0188d8ce9764c4a5
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-21 07:43:06 +0545

    feat: course chapters create and reorder

--- app/(dashboard)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapter-form";
 
 const CourseId = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -17,8 +18,11 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
   if (!userId) return redirect("/");
 
   const course = await db.course.findUnique({
-    where: { id: params.courseId },
+    where: { id: params.courseId, userId },
     include: {
+      chapters: {
+        orderBy: { position: "asc" },
+      },
       attachments: {
         orderBy: {
           createdAt: "desc",
@@ -34,7 +38,14 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
 
   if (!course) return redirect("/");
 
-  const requiredFields = [course.title, course.description, course.imageUrl, course.price, course.categoryId];
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
+  ];
 
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
@@ -75,9 +86,8 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
               <IconBadge icon={ListChecks} />
               <h2 className="text-xl">Course chapters</h2>
             </div>
-            <div>TODO: Chapters</div>
+            <ChaptersForm initialData={course} courseId={course.id} />
           </div>
-
           <div>
             <div className="flex items-center gap-x-2">
               <IconBadge icon={CircleDollarSign} />
@@ -85,7 +95,6 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
             </div>
             <PriceForm initialData={course} courseId={course.id} />
           </div>
-
           <div>
             <div className="flex items-center gap-x-2">
               <IconBadge icon={File} />

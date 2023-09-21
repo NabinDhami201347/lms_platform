e825162980dede483715a1b9cb8efb11ec3e0488
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-21 17:26:58 +0545

    feat: courses table tanstack tables

--- app/(dashboard)/teacher/courses/page.tsx
+++ app/(dashboard)/teacher/courses/page.tsx
@@ -1,12 +1,20 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { db } from "@/lib/db";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const Courses = () => {
+const Courses = async () => {
+  const { userId } = auth();
+  if (!userId) return redirectToSignIn();
+
+  const courses = await db.course.findMany({
+    where: { userId },
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };

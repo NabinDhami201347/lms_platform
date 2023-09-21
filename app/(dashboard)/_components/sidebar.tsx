12904eeda961ce4921ad59e6b374a425c33a7041
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-21 17:54:33 +0545

    feat: updated to 13.5

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,12 +1,12 @@
-import { Logo } from "./logo";
+import Link from "next/link";
 import { SidebarRoutes } from "./sidebar-routes";
 
 export const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-      <div className="p-6">
-        <Logo />
-      </div>
+      <Link href="/" className="p-5 font-semibold text-rose-600">
+        Creative Nepal
+      </Link>
 
       <div className="flex flex-col w-full">
         <SidebarRoutes />

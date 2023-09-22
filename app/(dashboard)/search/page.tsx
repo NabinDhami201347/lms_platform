c98b5ae0a85d0c9e3698cf92210082ddaee0da58
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-22 07:51:24 +0545

    feat: search and category select functionality

--- app/(dashboard)/search/page.tsx
+++ app/(dashboard)/search/page.tsx
@@ -1,5 +1,23 @@
-const Search = () => {
-  return <div>Search</div>;
+import { db } from "@/lib/db";
+
+import Categories from "./_components/categories";
+import SearchInput from "@/components/search-input";
+
+const Search = async () => {
+  const categories = await db.category.findMany({
+    orderBy: { name: "asc" },
+  });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
+  );
 };
 
 export default Search;
